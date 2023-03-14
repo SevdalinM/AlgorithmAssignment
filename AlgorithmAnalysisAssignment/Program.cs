dcd62cfa123610e23b805906fc09345d091a6622
SevdalinM <sevdalin0223@gmail.com>
2023-03-14 18:04:32 +0000

    a-g done

--- AlgorithmAnalysisAssignment/Program.cs
+++ AlgorithmAnalysisAssignment/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.Serialization;
+
 namespace AlgorithmAnalysisAssignment
 {
     class Program
@@ -11,11 +14,19 @@ namespace AlgorithmAnalysisAssignment
             ManageArrays manage = new ManageArrays();
             manage.populateArrays();
             //manage.displayArrays();
-            manage.algorithmA(manage.listOfArrays[0]);
+
+            //foreach (int[] array in manage.listOfArrays)
+            //{
+            //    manage.algorithmD(array);
+            //}
+
+            manage.algorithmG(manage.staticArray);
         }
 
         class ManageArrays 
         {
+            public int[] staticArray = { 6, 8, 9, 4, 5, 20, 10, 8, 12, 15, 13, 5 };
+
             public List<int[]> listOfArrays = new List<int[]>();
             public int[] arr8 = new int[8];
             public int[] arr16 = new int[16];
@@ -60,16 +71,261 @@ namespace AlgorithmAnalysisAssignment
 
             public void algorithmA(int[] array)
             {
-                int[] array1 = new int[array.Length/2];
-                int[] array2 = new int[array.Length/2];
-                array1=array.Take((array.Length) / 2).ToArray();
-                array2=array.Skip((array.Length) / 2).ToArray();
-                Console.WriteLine(array[0]);
+                int[] array1=array.Take((array.Length) / 2).ToArray();
+                int[] array2=array.Skip((array.Length) / 2).ToArray();
+
                 for (int i = 0; i < array1.Length; i++) 
                 {
                     Console.WriteLine("S1: " + array1[i]);
                 }
 
+                for (int i = 0; i < array2.Length; i++)
+                {
+                    Console.WriteLine("S2: " + array2[i]);
+                }
+            }
+
+            public void algorithmB(int[] array) 
+            {
+                int[] evenArray = new int[array.Length/2];
+                int[] oddArray = new int[array.Length/2];
+
+                for (int i = 0; i < array.Length; i++) 
+                {
+                    if (i % 2 == 0)
+                    {   //even
+                        evenArray[i/2]=array[i];
+                      
+                    }
+                    else
+                    {   //odd
+                        oddArray[i/2] = array[i];
+                    }
+                }
+
+                for (int i = 0; i < evenArray.Length; i++)
+                {
+                    Console.WriteLine("S1: " + evenArray[i]);
+                }
+
+                for (int i = 0; i < oddArray.Length; i++)
+                {
+                    Console.WriteLine("S2: " + oddArray[i]);
+                }
+            }
+
+            public void algorithmC(int[] array) 
+            {
+                int[] evenValues = new int[array.Length];
+                int[] oddValues = new int[array.Length];
+                int evenIndex=0;
+                int oddIndex=0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] % 2 == 0)
+                    {   //even
+                        evenValues[evenIndex] = array[i];
+                        evenIndex++;
+                    }
+                    else
+                    {   //odd
+                        oddValues[oddIndex] = array[i];
+                        oddIndex++;
+                    }
+                }
+                Array.Resize(ref evenValues, evenIndex);
+                Array.Resize(ref oddValues, oddIndex);
+                for (int i = 0; i < evenValues.Length; i++)
+                {
+                    Console.WriteLine("S1: " + evenValues[i]);
+                }
+
+                for (int i = 0; i < oddValues.Length; i++)
+                {
+                    Console.WriteLine("S2: " + oddValues[i]);
+                }
+            }
+
+            public void algorithmD(int[] array)  
+            {
+                int[] array1 = new int[array.Length];
+                int[] array2 = new int[array.Length];
+                array1[0] = array[0];
+                array2[0] = array[1];
+                int sumArr1 = array1.Sum();
+                int sumArr2= array2.Sum();
+                int arr1Index = 1;
+                int arr2Index = 1;
+              
+                for (int i = 2; i < array.Length; i++) 
+                {
+                    if (sumArr1 < sumArr2) 
+                    {
+                        array1[arr1Index] = array[i];
+                        arr1Index++;
+                        sumArr1 = array1.Sum();
+                    }
+                    else
+                    {
+                        array2[arr2Index] = array[i];
+                        arr2Index++;
+                        sumArr2 = array2.Sum();
+                    }
+                }
+
+                Array.Resize(ref array1, arr1Index);
+                Array.Resize(ref array2, arr2Index);
+
+                Console.WriteLine("The Sum of S1: " + array1.Sum());
+                for (int i = 0; i < array1.Length; i++)
+                {
+                    Console.WriteLine("S1: " + array1[i]);
+                }
+                Console.WriteLine("The Sum of S2: " + array2.Sum());
+                for (int i = 0; i < array2.Length; i++)
+                {
+                    Console.WriteLine("S2: " + array2[i]);
+                }
+
+                
+            }
+
+            public void algorithmE(int[] array) 
+            {
+                Array.Sort(array);
+                int[] evenArray = new int[array.Length / 2];
+                int[] oddArray = new int[array.Length / 2];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {   //even
+                        evenArray[i / 2] = array[i];
+
+                    }
+                    else
+                    {   //odd
+                        oddArray[i / 2] = array[i];
+                    }
+                }
+
+                for (int i = 0; i < evenArray.Length; i++)
+                {
+                    Console.WriteLine("S1: " + evenArray[i]);
+                }
+
+                for (int i = 0; i < oddArray.Length; i++)
+                {
+                    Console.WriteLine("S2: " + oddArray[i]);
+                }
+            }
+
+            public void algorithmF(int[] array) 
+            {
+                Array.Sort (array);
+
+                int[] array1 = new int[array.Length];
+                int[] array2 = new int[array.Length];
+                array1[0] = array[0];
+                array2[0] = array[1];
+                int sumArr1 = array1.Sum();
+                int sumArr2 = array2.Sum();
+                int arr1Index = 1;
+                int arr2Index = 1;
+
+                for (int i = 2; i < array.Length; i++)
+                {
+                    if (sumArr1 < sumArr2)
+                    {
+                        array1[arr1Index] = array[i];
+                        arr1Index++;
+                        sumArr1 = array1.Sum();
+                    }
+                    else
+                    {
+                        array2[arr2Index] = array[i];
+                        arr2Index++;
+                        sumArr2 = array2.Sum();
+                    }
+                }
+
+                Array.Resize(ref array1, arr1Index);
+                Array.Resize(ref array2, arr2Index);
+
+                Console.WriteLine("The Sum of S1: " + array1.Sum());
+                for (int i = 0; i < array1.Length; i++)
+                {
+                    Console.WriteLine("S1: " + array1[i]);
+                }
+                Console.WriteLine("The Sum of S2: " + array2.Sum());
+                for (int i = 0; i < array2.Length; i++)
+                {
+                    Console.WriteLine("S2: " + array2[i]);
+                }
+            }
+
+            public void algorithmG(int[] array)
+            {
+                Array.Sort(array);
+                Array.Reverse(array);
+
+                int[] array1 = new int[array.Length];
+                int[] array2 = new int[array.Length];
+                array1[0] = array[0];
+                array2[0] = array[1];
+                int sumArr1 = array1.Sum();
+                int sumArr2 = array2.Sum();
+                int arr1Index = 1;
+                int arr2Index = 1;
+
+                for (int i = 2; i < array.Length; i++)
+                {
+                    if (sumArr1 < sumArr2)
+                    {
+                        array1[arr1Index] = array[i];
+                        arr1Index++;
+                        sumArr1 = array1.Sum();
+                    }
+                    else
+                    {
+                        array2[arr2Index] = array[i];
+                        arr2Index++;
+                        sumArr2 = array2.Sum();
+                    }
+                }
+
+                Array.Resize(ref array1, arr1Index);
+                Array.Resize(ref array2, arr2Index);
+
+                Console.WriteLine("The Sum of S1: " + array1.Sum());
+                for (int i = 0; i < array1.Length; i++)
+                {
+                    Console.WriteLine("S1: " + array1[i]);
+                }
+                Console.WriteLine("The Sum of S2: " + array2.Sum());
+                for (int i = 0; i < array2.Length; i++)
+                {
+                    Console.WriteLine("S2: " + array2[i]);
+                }
+            }
+
+            public void algorithmH(int[] array) 
+            {
+                int idealSumPartition = array.Sum()/2;
+
+                int[] array1 = new int[array.Length];
+                int[] array2 = new int[array.Length];
+                int sumArr1 = 0;
+                int sumArr2 = 0;
+ 
+                
+
+                Console.WriteLine("The Sum of S1: " + array1.Sum());
+                for (int i = 0; i < array1.Length; i++)
+                {
+                    Console.WriteLine("S1: " + array1[i]);
+                }
+                Console.WriteLine("The Sum of S2: " + array2.Sum());
                 for (int i = 0; i < array2.Length; i++)
                 {
                     Console.WriteLine("S2: " + array2[i]);
